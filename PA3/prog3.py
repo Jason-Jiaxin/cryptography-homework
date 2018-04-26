94deb83a5890bd2a357855c44953b2568dbb011f
Jiaxin Yang <jiaxinn.yang@gmail.com>
2018-04-25 22:41:21 -0400

    implementing blockchain

--- PA3/prog3.py
+++ PA3/prog3.py
@@ -1,4 +1,4 @@
-import random, math, hashlib, sys
+import random, math, hashlib, sys, json
 sys.setrecursionlimit(1000000)
 
 # Implement Miller-Rabin Primality test
@@ -61,7 +61,7 @@ def mulinv(b, n):
 
 # Return (pk, sk, N)
 def generateKey():
-    n = 1024
+    n = 64
     p = generate_prime(n)
     q = generate_prime(n)
     # RSA modulus N = pq
@@ -125,7 +125,114 @@ def verPuzzle(s, x, n):
     zeroString = '0' * n
     return hashSX(s, x).startswith(zeroString)
 
+NUM_ZEROS = 10
+
 class Ledger:
+
+    def __init__(self):
+        self.userCoins = {}
+        self.coinIndex = 1
+        self.blocks = []
+        self.blockIndex = 0
+        self.initTransQueue()
 
     def createUser(self):
         (pk, sk, N) = generateKey()
+        self.userCoins[str(pk)] = set()
+        return self.User(pk, sk, N)
+
+    def initLedger(self, user):
+        t = self.genTransaction(user, user.pk, self._mintCoins(10), True)
+        b = self.Block(self.blockIndex, b'0', [t])
+        self.blockIndex += 1
+        b.verified = True
+        self.blocks.append(b)
+        self._processTransactions(b.transactions)
+
+    def initTransQueue(self):
+        self.tq = []
+
+    def genTransaction(self, userSend, pkr, coins, isReturn):
+        message = bytearray()
+        message += bytearray(intToBytes(userSend.pk))
+        message += bytearray(intToBytes(pkr))
+        for c in coins:
+            message += bytearray(intToBytes(c))
+        # print('message', message)
+        signature = sign(message, userSend.sk, userSend.N)
+        transaction = self.Transaction(userSend.pk, userSend.N, pkr, coins, signature)
+        if (isReturn):
+            return transaction
+        else:
+            self.tq.append(transaction)
+
+    def genBlock(self, user, T, n):
+        trans = []
+        while len(self.tq) > 0 and T > 0:
+            trans.append(self.tq.pop(0))
+            T -= 1
+        trans.append(self.genTransaction(user, user.pk, self._mintCoins(10), True))
+        b = self.Block(self.blockIndex, self.blocks[-1].blockHash, trans)
+        self.blockIndex += 1
+        salt = solvePuzzle(b.prevBlockHash + b.blockHash, n)
+        b.solution = salt
+        self.blocks.append(b)
+
+    def verBlock(self, block, n):
+        pass
+
+    def checkBalance(self, pku):
+        print('User', pku, 'Num:', len(self.userCoins[str(pku)]), 'Coins:', self.userCoins[str(pku)])
+
+    def userBalance(self):
+        print('All user balance')
+        for k, v in self.userCoins.items():
+            print('User', k, 'Num:', len(v), 'Coins:', v)
+        print()
+
+    def _processTransactions(self, transactions):
+        for t in transactions:
+            if (t.pks == t.pkr):
+                self.userCoins[str(t.pkr)].update(t.coins)
+            else:
+                self.userCoins[str(t.pkr)].update(t.coins)
+                self.userCoins[str(t.pks)].difference_update(t.coins)
+
+    def _mintCoins(self, n):
+        coins = []
+        for i in range(n):
+            coins.append(self.coinIndex)
+            self.coinIndex += 1
+        return coins
+
+    class User:
+        def __init__(self, pk, sk, N):
+            self.pk = pk
+            self.sk = sk
+            self.N = N
+
+    class Transaction:
+        def __init__(self, pks, N, pkr, coins, signature):
+            self.pks = pks
+            self.N = N
+            self.pkr = pkr
+            self.coins = coins
+            self.signature = signature
+
+    class Block:
+        def __init__(self, index, prevBlockHash, transactions):
+            self.index = index
+            self.prevBlockHash = prevBlockHash
+            self.transactions = transactions
+            self.verified = False
+            self.solution = 0
+            self._calculateHash()
+
+        def _calculateHash(self):
+            signatures = [intToBytes(t.signature) for t in self.transactions]
+            self.blockHash = sha256(*signatures)
+
+        def printSelf(self):
+            print('Block:', self.index, 'Prev block hash:', self.prevBlockHash,
+                  'Num transactions', len(self.transactions), 'Block hash:', self.blockHash,
+                  'Solution:', self.solution, 'Verified:', self.verified)
--- PA3/prog3test.py
+++ PA3/prog3test.py
@@ -33,3 +33,28 @@ def testPoW():
         print()
 
 # testPoW()
+pk = 24824973012987653108813315472142148812868167137203560813408237508995350164639
+sk = 55681588644576920927153194286920066383164290810445193335097792020100461327855
+N = 81711360432909513721989947016618208086222908053157068342007920422176201253541
+NUM_ZEROS = 10
+
+def testBlockChain():
+    l = prog3.Ledger()
+    # u = l.User(pk, sk, N)
+    u1 = l.createUser()
+    u2 = l.createUser()
+    u3 = l.createUser()
+    # print(type(u.pk), prog3.intToBytes(u.pk))
+    # t = l.genTransaction(u, u.pk, l._mintCoins(10))
+    # print(t.coins)
+    # print(t.signature)
+    # b = l.Block(1, 0, [t])
+    # print(b.blockHash)
+    l.initLedger(u1)
+    l.userBalance()
+    l.genTransaction(u1, u2.pk, [1, 2], False)
+    l.genTransaction(u1, u3.pk, [5, 6], False)
+    l.genBlock(u2, 2, NUM_ZEROS)
+    l.blocks[-1].printSelf()
+
+testBlockChain()
